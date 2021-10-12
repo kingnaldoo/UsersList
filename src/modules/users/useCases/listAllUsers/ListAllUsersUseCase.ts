b40f3c2bc1c7b7e73cdb4a16941c597dd1e857d9
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-12 15:10:40 -0300

    challenge finalized

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("This user has not permission to access");
+    }
+
+    console.log(user);
+    const list = this.usersRepository.list();
+
+    return list;
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,15 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User profile is not exists");
+    }
+
+    console.log(user);
+
+    return user;
   }
 }
 
